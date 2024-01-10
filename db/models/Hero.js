7ae29e665f62fa1eaf3ac9ae127d854b91bfae28
OguzKabasakal <oguzkabasakal@gmail.com>
2024-01-10 12:35:41 +0100

    set up db folder and next.js config

---
+++ db/models/Hero.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const heroSchema = new Schema({
+  rank: { type: String, required: true },
+  heroName: { type: String, required: true },
+  alias: { type: String, required: true },
+  realName: { type: String, required: true },
+  powers: { type: Array, required: true },
+  imgURL: { type: String, required: true}
+});
+
+const Hero = mongoos.models.Hero || mongoose.model("Hero", heroSchema);
+
+export default Hero;
